9c771d140ffb96424faf3f6435cdd3a6225849b3
Vikram Pawar <vikram@insignia.tech>
2025-05-21 00:44:17 +0530

    Sidebar UI Changes for mobile

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Sidebar } from "primereact/sidebar";
 import "./header.css";
-import logo from "../../Assets/Images/logo.png";
+import logo from "../../Assets/Images/BWC-Logo.png";
+import SidebarUI from "../Sidebar/Sidebar";
 
 const Header = () => {
+  const [visibleLeft, setVisibleLeft] = useState(false);
   return (
-    <header className="header">
-    <img src={logo} alt="Logo" className="logo" />
-  </header>
+    <>
+      <header className="header">
+        <div className="header__logo">
+          <img src={logo} alt="Logo" className="logo" />
+          <span>Admin</span>
+        </div>
+        <div className="hamburger-btn">
+          <label class="switch" onClick={() => setVisibleLeft(true)}>
+            <input type="checkbox" />
+            <span class="wrapper">
+              <span class="row">
+                <span class="dot"></span>
+                <span class="dot"></span>
+              </span>
+              <span class="row row-bottom">
+                <span class="dot"></span>
+                <span class="dot"></span>
+              </span>
+              <span class="row-vertical">
+                <span class="dot"></span>
+                <span class="dot middle-dot"></span>
+                <span class="dot"></span>
+              </span>
+              <span class="row-horizontal">
+                <span class="dot"></span>
+                <span class="dot middle-dot-horizontal"></span>
+                <span class="dot"></span>
+              </span>
+            </span>
+          </label>
+        </div>
+      </header>
+      <Sidebar
+        header="BWC Admin"
+        visible={visibleLeft}
+        position="left"
+        onHide={() => setVisibleLeft(false)}
+      >
+        <SidebarUI />
+      </Sidebar>
+    </>
   );
 };
 export default Header;
